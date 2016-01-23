f1e80212435333d53c05abbc0fdf9cbd4cb5d7f2
Boris Iskra <borisiskra@gmail.com>
2016-01-23 00:58:02 -0800

    complete assignmet

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,15 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions create a "cached matrix inverse",
+## initialy the value of the nverse is set to NULL. The first time
+## the inverse is need is calculated and stored in the field "x_inv"
+## after this first calculation, any attempt to get the inverse again
+## will result in returning the "cached" value, so sparing a second
+## calculation of the same inverse.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates an "object" with two fields, the matrix x,
+## an the inverse x_inv, and four methods "get" and "set" to get and  
+## set the value of the matrix, and "getinv" and "setinv" to get and 
+## the value of the inverse. The initial value of "x_inv" is NULL.
+##
 
 makeCacheMatrix <- function(x = matrix()) {
     x_inv <- NULL
@@ -18,7 +26,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve tries to get the inverse of the matrix x, from
+## its cached value. If it is NULL, then is calculated,
+## if it is not NULL is returned from its cached value via
+## the "getinv" method.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
